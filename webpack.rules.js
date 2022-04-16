71ae0142f4dd68f4ea5c18a5385dc48edb624e4a
Clovis de Villars <clovis.devillars@gmail.com>
2022-04-16 18:23:58 +0200

    Fix webpack config to support native modules

--- webpack.rules.js
+++ webpack.rules.js
@@ -4,24 +4,24 @@ module.exports = [
     test: /\.node$/,
     use: 'node-loader',
   },
-  {
-    test: /\.(m?js|node)$/,
-    parser: { amd: false },
-    use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
-      options: {
-        outputAssetBase: 'native_modules',
-      },
-    },
-  },
+  // {
+  //   test: /\.(m?js|node)$/,
+  //   parser: { amd: false },
+  //   use: {
+  //     loader: '@vercel/webpack-asset-relocator-loader',
+  //     options: {
+  //       // outputAssetBase: 'native_modules',
+  //     },
+  //   },
+  // },
   {
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
     use: {
       loader: 'ts-loader',
       options: {
-        transpileOnly: true
-      }
-    }
+        transpileOnly: true,
+      },
+    },
   },
-];
+]
