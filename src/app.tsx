aea540873b9946181724245f31bf544b9bd26223
Clovis de Villars <clovis.devillars@gmail.com>
2022-04-16 18:44:04 +0200

    Setup Material-UI

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import theme from './theme'
 
 const root = createRoot(document.getElementById('root'))
 
-root.render(<h2>Hello from React!</h2>)
+root.render(
+  <ThemeProvider theme={ theme }>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */ }
+    <CssBaseline/>
+    <h2>Hello from React!</h2>
+  </ThemeProvider>,
+)
