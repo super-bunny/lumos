6aec79e5f7bbf68da188cc596c4abd22edb99ada
Clovis de Villars <clovis.devillars@gmail.com>
2022-07-08 18:45:26 +0200

    Improve Center component compatibility with material-ui

--- src/components/atoms/Center.tsx
+++ src/components/atoms/Center.tsx
@@ -1,15 +1,19 @@
 import React, { PropsWithChildren } from 'react'
+import { Box, SxProps } from '@mui/material'
 
-export default function Center({ children }: PropsWithChildren<{}>) {
+export interface Props {
+  sx?: SxProps
+}
+
+export default function Center({ sx, children }: PropsWithChildren<Props>) {
   return (
-    <div
-      style={ {
-        height: '100%',
-        width: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      } }
-    >{ children }</div>
+    <Box
+      height={ 1 }
+      width={ 1 }
+      display={ 'flex' }
+      justifyContent={ 'center' }
+      alignItems={ 'center' }
+      sx={ sx }
+    >{ children }</Box>
   )
 }
