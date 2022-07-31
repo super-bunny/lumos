de6d43106c7c3b926fa007754f33ff0e27ed3512
Clovis de Villars <clovis.devillars@gmail.com>
2022-08-01 00:21:30 +0200

    Fix webpack config to avoid stuck during packaging

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -2,10 +2,13 @@ const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-module.exports = [
+const plugins = [
   new ForkTsCheckerWebpackPlugin(),
   new ReactRefreshWebpackPlugin(),
-  new BundleAnalyzerPlugin({
-    openAnalyzer: process.env.OPEN_ANALYZER === 'true',
-  }),
 ]
+
+if (process.env.OPEN_ANALYZER === 'true') {
+  plugins.push(new BundleAnalyzerPlugin())
+}
+
+module.exports = plugins
