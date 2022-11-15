b35087113e1e8aae1a8fb2f9bc8e68fd83b9c4ad
Clovis de Villars <clovis.devillars@gmail.com>
2022-11-15 11:39:23 +0100

    Change default window size

--- src/main/index.ts
+++ src/main/index.ts
@@ -21,8 +21,8 @@ export default function main() {
   const createWindow = (): BrowserWindow => {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-      height: 600,
-      width: 800,
+      height: 720,
+      width: 1280,
       webPreferences: {
         preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
         nodeIntegration: false,
