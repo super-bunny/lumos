60409cb894974ebd84e8b9e1e09fcfefacf19b3f
Clovis de Villars <clovis.devillars@gmail.com>
2022-12-22 23:39:07 +0100

    fix: apply animation setting for setting page animation

--- src/renderer/components/pages/SettingsPage.tsx
+++ src/renderer/components/pages/SettingsPage.tsx
@@ -3,11 +3,15 @@ import { Button, Container, Grow, Stack, Typography } from '@mui/material'
 import Settings from '../organisms/Settings'
 import { useNavigate } from 'react-router-dom'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import useSettingsStore from '../../hooks/useSettingsStore'
 
 export default function SettingsPage() {
   const navigate = useNavigate()
+
+  const { settingsStore } = useSettingsStore()
+
   return (
-    <Grow in={ true } timeout={ 500 }>
+    <Grow in={ true } appear={ settingsStore?.settings.enableAnimations ?? false } timeout={ 500 }>
       <Container maxWidth={ 'lg' } sx={ { height: 1, py: 4, flexGrow: 1 } }>
         <Stack height={ 1 }>
           <Stack mb={ 4 } direction={ 'row' } justifyContent={ 'space-between' } gap={ 1 }>
