eb2902f39c622ea29fefe45e32aa26a00379af43
Clovis de Villars <clovis.devillars@gmail.com>
2023-02-23 22:44:27 +0100

    feat: add update method to settings store hook

--- src/renderer/hooks/useSettingsStore.ts
+++ src/renderer/hooks/useSettingsStore.ts
@@ -1,15 +1,26 @@
 import useSwr from 'swr'
+import { useCallback } from 'react'
+import Settings from '../../types/Settings'
 
 const KEY = 'settings'
 
 export default function useSettingsStore() {
-  const { data, ...rest } = useSwr(KEY, () => window.lumos.store.getSettings(), {
+  const { data, mutate, ...rest } = useSwr(KEY, () => window.lumos.store.getSettings(), {
     revalidateOnReconnect: false,
     revalidateOnFocus: false,
   })
 
+  const updateSettings = useCallback(async (partialSettings: Partial<Settings>) => {
+    if (!data) return
+    const newSettings = { ...data.settings, ...partialSettings }
+    await mutate({ ...data, settings: newSettings }, { revalidate: false })
+    return window.lumos.store.setSettings(newSettings)
+  }, [data, mutate])
+
   return {
     settingsStore: data,
+    mutate,
     ...rest,
+    updateSettings,
   }
 }
