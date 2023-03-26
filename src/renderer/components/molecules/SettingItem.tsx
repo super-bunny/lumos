0b395c5b434753cc58605b5a82d6fe56b5ba3591
Clovis de Villars <clovis.devillars@gmail.com>
2023-03-26 14:34:52 +0200

    chore: improve <SettingItem/> component
    Allow react element for label and description.
    Transform the "label" element into html label.
    Add a prop to pass a "for" attribute to the new label element inside the component.
    Make the label element fill all available width to allow the user to click anywhere in the label+action area.
    Make description support new line characters.

--- src/renderer/components/molecules/SettingItem.tsx
+++ src/renderer/components/molecules/SettingItem.tsx
@@ -2,20 +2,21 @@ import React, { ReactNode } from 'react'
 import { Stack, Typography } from '@mui/material'
 
 export interface Props {
-  label: string
-  description?: string
+  label: ReactNode
+  labelFor?: string
+  description?: ReactNode
   action: ReactNode
 }
 
-export default function SettingItem({ label, description, action }: Props) {
+export default function SettingItem({ label, labelFor, description, action }: Props) {
   return (
     <Stack>
       <Stack direction={ 'row' } alignItems={ 'center' } justifyContent={ 'space-between' }>
-        <Typography >{ label }</Typography>
+        <label htmlFor={ labelFor } style={ { flexGrow: 1 } }>{ label }</label>
         { action }
       </Stack>
 
-      <Typography variant={ 'body2' } color={'text.secondary'}>{ description }</Typography>
+      <Typography variant={ 'body2' } color={ 'text.secondary' } whiteSpace={ 'pre-line' }>{ description }</Typography>
     </Stack>
   )
 }
