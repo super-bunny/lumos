90d97fae86881bc59d858ffc7634119a65607ea0
Clovis de Villars <clovis.devillars@gmail.com>
2023-06-06 21:15:04 +0200

    feat(overlay): add fade in/out animation for overlay

--- src/overlay/components/BrightnessSlider.tsx
+++ src/overlay/components/BrightnessSlider.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 import { Slider, Stack, Typography } from '@mui/material'
 import { IpcEvents } from '../../types/Ipc'
 import { IpcDisplayUpdateArgs } from '../../main/utils/ipc'
@@ -6,18 +6,54 @@ import VCPFeatures from '../../types/VCPFeatures'
 import { VcpValueType } from '../../types/EnhancedDDCDisplay'
 import BrightnessMediumIcon from '@mui/icons-material/BrightnessMedium'
 import { useAppSelector } from '../store/store'
-
-export interface Props {
-}
+import asyncTimeout from '../../shared/utils/asyncTimeout'
+import useSettingsStore from '../../renderer/hooks/useSettingsStore'
 
 const TIMEOUT = 2000
+const root = document.getElementById('root')
 
-export default function BrightnessSlider({}: Props) {
+function showWindow(animate: boolean) {
+  return window.lumos.setWindowVisibility(true)
+    .then(() => {
+      if (root) {
+        root.className = animate ? 'shown' : ''
+      }
+    })
+}
+
+async function hideWindow(animate: boolean) {
+  if (animate) {
+    if (root) root.className = 'hidden'
+    await asyncTimeout(400)
+  }
+  return window.lumos.setWindowVisibility(false)
+}
+
+export default function BrightnessSlider() {
+  const { settingsStore } = useSettingsStore()
+  const enableAnimations = useMemo<boolean>(() => settingsStore?.settings.enableAnimations !== false, [settingsStore])
+
   const { displayId } = useAppSelector(state => state.overlayInfo) ?? {}
   const [brightness, setBrightness] = useState<number>(0)
   const [hideTimeoutId, setHideTimeoutId] = useState<number>()
 
   const { showDisplayId = false } = window.lumos.initSettings.overlay
+
+  const handleBrightnessChange = useCallback((brightness: number) => showWindow(enableAnimations)
+    .then(() => {
+      setBrightness(brightness)
+    })
+    .then(() => {
+      if (window.lumos.env.FORCE_SHOW_OVERLAY === 'true') return
+      if (hideTimeoutId) clearTimeout(hideTimeoutId)
+
+      const timeoutId = window.setTimeout(() => {
+        if (window.lumos.env.NODE_ENV === 'development' && window.lumos.env.PROD_OVERLAY !== 'true') return
+        return hideWindow(enableAnimations)
+      }, TIMEOUT)
+
+      setHideTimeoutId(timeoutId)
+    }), [enableAnimations, hideTimeoutId])
 
   useEffect(() => {
     const listener = window.lumos.ipc.on(IpcEvents.DISPLAY_UPDATE, ({
@@ -25,34 +61,23 @@ export default function BrightnessSlider({}: Props) {
       vcpFeature,
       vcpValue,
     }: IpcDisplayUpdateArgs) => {
-      console.debug('Received display update:', displayId, vcpFeature, vcpValue)
+      console.debug('Received display update:', eventDisplayId, vcpFeature, vcpValue)
 
       if (
         eventDisplayId !== displayId
         || vcpFeature !== VCPFeatures.ImageAdjustment.Luminance
         || vcpValue?.type !== VcpValueType.Continuous
         || vcpValue === undefined
-      ) return
+      ) {
+        console.debug('Display update ignored:', eventDisplayId, vcpFeature, vcpValue)
+        return
+      }
 
-      window.lumos.setWindowVisibility(true)
-        .then(() => {
-          setBrightness(Math.round(vcpValue.currentValue * 100 / vcpValue.maximumValue))
-        })
-        .then(() => {
-          if (window.lumos.env.FORCE_SHOW_OVERLAY === 'true') return
-          if (hideTimeoutId) clearTimeout(hideTimeoutId)
-
-          const timeoutId = window.setTimeout(() => {
-            if (window.lumos.env.NODE_ENV === 'development' && window.lumos.env.PROD_OVERLAY !== 'true') return
-            return window.lumos.setWindowVisibility(false)
-          }, TIMEOUT)
-
-          setHideTimeoutId(timeoutId)
-        })
+      handleBrightnessChange(Math.round(vcpValue.currentValue * 100 / vcpValue.maximumValue)).then()
     })
 
     return () => window.lumos.ipc.removeListener(IpcEvents.DISPLAY_UPDATE, listener)
-  }, [displayId, hideTimeoutId])
+  }, [displayId, handleBrightnessChange])
 
   return (
     <Stack alignItems={ 'stretch' } justifyContent={ 'center' } style={ { height: '100%', position: 'relative' } }>
