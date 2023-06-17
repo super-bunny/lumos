60a52d399af3e044a1959f11e61ed28dbbf1a519
Clovis de Villars <clovis.devillars@gmail.com>
2023-06-17 20:00:03 +0200

    deps: update build related deps like electron forge and webpack plugins

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -4,18 +4,20 @@ const rendererPlugins = require('./webpack.renderer.plugins')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-rules.push({
-  test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-})
-
-rules.push({
-  test: /\.(ttf|eot|woff|woff2)$/,
-  loader: 'file-loader',
-  options: {
-    name: 'fonts/[name].[ext]',
+rules.push(
+  {
+    test: /\.css$/,
+    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
   },
-})
+  {
+    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+  },
+  {
+    test: /\.(ttf|eot|woff|woff2)$/,
+    type: 'asset/resource',
+  },
+)
 
 module.exports = {
   devtool: 'source-map',
