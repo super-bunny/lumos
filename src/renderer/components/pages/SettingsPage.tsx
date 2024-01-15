fae95770939a248301fc41adae2d7f2fe527d66b
Clovis de Villars <clovis.devillars@gmail.com>
2024-01-16 00:21:44 +0100

    chore(settings): change icon and label of go back button

--- src/renderer/components/pages/SettingsPage.tsx
+++ src/renderer/components/pages/SettingsPage.tsx
@@ -2,8 +2,8 @@ import * as React from 'react'
 import { Button, Container, Grow, Stack, Typography } from '@mui/material'
 import Settings from '../organisms/Settings'
 import { useNavigate } from 'react-router-dom'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 import useSettingsStore from '../../hooks/useSettingsStore'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 
 export default function SettingsPage() {
   const navigate = useNavigate()
@@ -14,10 +14,15 @@ export default function SettingsPage() {
     <Grow in={ true } appear={ settingsStore?.settings.enableAnimations ?? false } timeout={ 500 }>
       <Container maxWidth={ 'lg' } sx={ { height: 1, py: 2, flexGrow: 1 } }>
         <Stack height={ 1 }>
-          <Stack mb={ 2 } direction={ 'row' } justifyContent={ 'space-between' } gap={ 1 }>
+          <Stack
+            mb={ 2 }
+            direction={ 'row' }
+            justifyContent={ 'space-between' }
+            gap={ 1 }
+          >
             <Typography variant={ 'h4' }>Settings</Typography>
 
-            <Button onClick={ () => navigate('/') } startIcon={ <ArrowBackIcon/> }>Go back to home</Button>
+            <Button onClick={ () => navigate('/') } endIcon={ <HighlightOffIcon/> }>Close</Button>
           </Stack>
 
           <Settings sx={ { flexGrow: 1, overflow: 'hidden' } }/>
